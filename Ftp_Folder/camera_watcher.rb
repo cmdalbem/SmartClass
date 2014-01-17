3a707cd52bee3c68e6a23f437467569a9d64b5e0
Julien Fleury <julien.fleury@ensimag.fr>
2014-01-17 16:47:06 +0100

    commentaires

--- Ftp_Folder/camera_watcher.rb
+++ Ftp_Folder/camera_watcher.rb
@@ -1,26 +1,28 @@
-require 'rb-inotify'
-require 'rest-client'
-require 'date'
+require 'rb-inotify' # Gem to check changes on folders and files
+require 'rest-client' # Gem to make HTTP requests
+require 'date' # Gem to manipulate time
 
 notifier = INotify::Notifier.new
 last_alarm = nil
 
 notifier.watch("camera", :create) do |event|
+  # Called each time an image is sent
   now = DateTime.now
   if !last_alarm || now.to_time - last_alarm.to_time > 10 # in seconds
+    # Do not send alarm if it has already been sent in the 10 previous seconds
     last_alarm = now
-    # Send a message to the openHAB server
+    puts "Alarm !"
+    # Can send a message directly to the openHAB server
     #RestClient.put 'http://ensipc03.imag.fr:8080/rest/items/Mouvement_Detector/state', "ON", {:content_type => 'text/plain'}
-    #RestClient.put 'http://localhost:8080/rest/items/Mouvement_Detector/state', "ON", {:content_type => 'text/plain'}
-    # Send a message to a machine connected to the VPN network
-    #RestClient.get 'http://localhost:3000/presence'
+    # Send a message to a machine connected to the VPN network -> here itself. The server redistribute the message to OpenHAB
+    RestClient.get 'http://localhost/presence'
   end
   # Delete the file if the presence is normal
   if now.hour < 20 && now.hour >= 7 && now.cwday <= 5
     File.delete("camera/"+event.name)
     puts "#{event.name} has been deleted after create."
   else
-    puts "#{event.name} is now in camera folder!"
+    puts "#{event.name} is kept in camera folder."
   end
 end
 
